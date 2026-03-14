fce766478c72350c076663edce0b35f30d86774d
sarathbabu7337 <dupanavamsi0@gmail.com>
2026-03-14 12:39:47 +0530

    Add files via upload

---
+++ script .js
@@ -0,0 +1,21 @@
+function appendValue(val) {
+  document.getElementById("display").value += val;
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+
+function deleteLast() {
+  let display = document.getElementById("display");
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    let result = eval(document.getElementById("display").value);
+    document.getElementById("display").value = result;
+  } catch {
+    document.getElementById("display").value = "Error";
+  }
+}
